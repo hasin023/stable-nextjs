8f4c9c39175eacde16db0bb3d93b0409c7059e73
Hasin <hasinmahtab.alvee@gmail.com>
2024-06-21 17:13:20 +0600

    Added download button

--- app/page.tsx
+++ app/page.tsx
@@ -1,134 +1,153 @@
-"use client"
+"use client";
 
-import { useState, FormEvent } from "react"
-import Image from "next/image"
-import Link from "next/link"
-import { generateImage } from "@/utils/hf-handlers"
+import { useState, type FormEvent } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { MdOutlineDownloading } from "react-icons/md";
+
+import { generateImage } from "@/utils/hf-handlers";
 
 function Home(): JSX.Element {
-  const [inputValue, setInputValue] = useState<string>(
-    "An astronaut riding a rainbow unicorn, cinematic, dramatic"
-  )
-  const [negativeInput, setnegativeInput] = useState<string>("")
-  const [imageUrl, setImageUrl] = useState<string>("")
-  const [loading, setLoading] = useState<boolean>(false)
-  const [isTextareaFocused, setIsTextareaFocused] = useState<boolean>(false)
+	const [inputValue, setInputValue] = useState<string>(
+		"An astronaut riding a rainbow unicorn, cinematic, dramatic",
+	);
+	const [negativeInput, setnegativeInput] = useState<string>("");
+	const [imageUrl, setImageUrl] = useState<string>("");
+	const [loading, setLoading] = useState<boolean>(false);
+	const [isTextareaFocused, setIsTextareaFocused] = useState<boolean>(false);
 
-  const handleSubmit = async (
-    event: FormEvent<HTMLFormElement>
-  ): Promise<void> => {
-    event.preventDefault()
-    setLoading(true)
+	const downloadImage = () => {
+		const a = document.createElement("a");
+		a.href = imageUrl;
+		a.download = `generated-${new Date().getTime()}.png`;
+		a.click();
+	};
 
-    const response = await generateImage(inputValue, negativeInput)
+	const handleSubmit = async (
+		event: FormEvent<HTMLFormElement>,
+	): Promise<void> => {
+		event.preventDefault();
+		setLoading(true);
 
-    if (response) {
-      const imageUrl = URL.createObjectURL(response)
-      setImageUrl(imageUrl)
-    } else {
-      console.error("Error:", response)
-    }
-    setLoading(false)
-  }
+		const response = await generateImage(inputValue, negativeInput);
 
-  const handleTextareaFocus = () => {
-    setIsTextareaFocused(true)
-  }
+		if (response) {
+			const imageUrl = URL.createObjectURL(response);
+			setImageUrl(imageUrl);
+		} else {
+			console.error("Error:", response);
+		}
+		setLoading(false);
+	};
 
-  const handleTextareaBlur = () => {
-    setIsTextareaFocused(false)
-  }
+	const handleTextareaFocus = () => {
+		setIsTextareaFocused(true);
+	};
 
-  return (
-    <div className='min-h-screen bg-gray-200 py-4 flex flex-col justify-center sm:py-10'>
-      <div className='relative py-1 sm:max-w-xl sm:mx-auto'>
-        <div className='relative px-6 py-8 bg-white shadow-lg sm:rounded-lg sm:py-6 sm:px-4'>
-          <form onSubmit={handleSubmit} className='max-w-md mx-auto'>
-            <textarea
-              rows={2}
-              cols={20}
-              wrap=''
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-              onFocus={handleTextareaFocus}
-              onBlur={handleTextareaBlur}
-              className='w-full px-3 py-2 text-gray-700 bg-gray-200 rounded focus:outline-none focus:ring focus:border-cyan-400'
-              placeholder='Enter your imagination...'
-            />
-            <input
-              type='text'
-              value={negativeInput}
-              onChange={(e) => setnegativeInput(e.target.value)}
-              className='w-full px-5 py-3 text-gray-700 bg-gray-200 rounded focus:outline-none focus:ring focus:border-cyan-400'
-              placeholder='Enter a negative prompt'
-            />
-            <button
-              type='submit'
-              className={`w-full mt-5 mb-1 px-3 py-2 text-white bg-gradient-to-r from-cyan-400 to-green-500 rounded-md focus:outline-none ${
-                isTextareaFocused ? "mt-2" : "mt-0"
-              }`}
-              disabled={loading}
-            >
-              Generate
-            </button>
-          </form>
-          <div className='flex justify-between -mb-2'>
-            <Link
-              href='/image/refine'
-              className='text-gray-600 text-sm px-2 py-1 hover:underline hover:text-lime-700'
-            >
-              Refine an image &rarr;
-            </Link>
-            <Link
-              href='/image/detect'
-              className='text-gray-600 text-sm px-2 py-1 hover:underline hover:text-lime-700'
-            >
-              Detect an image &rarr;
-            </Link>
-            <Link
-              href='/image/answer'
-              className='text-gray-600 text-sm px-2 py-1 hover:underline hover:text-lime-700'
-            >
-              Answer question &rarr;
-            </Link>
-          </div>
+	const handleTextareaBlur = () => {
+		setIsTextareaFocused(false);
+	};
 
-          <div className='flex justify-center -mb-2 mt-1'>
-            <Link
-              href='/audio/transcriber'
-              className='text-gray-600 text-sm px-2 py-1 hover:underline hover:text-cyan-700'
-            >
-              Audio transcriber &rarr;
-            </Link>
-            <Link
-              href='/audio/generate'
-              className='text-gray-600 text-sm px-2 py-1 hover:underline hover:text-cyan-700'
-            >
-              Generate audio &rarr;
-            </Link>
-          </div>
-        </div>
-      </div>
-      {loading && (
-        <div className='mt-12 flex justify-center'>
-          <div className='loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12'></div>
-        </div>
-      )}
-      {imageUrl && !loading && (
-        <div className='mt-12 flex justify-center'>
-          <Image
-            src={imageUrl}
-            alt='Generated image'
-            className='rounded-lg shadow-lg'
-            height={350}
-            width={350}
-          />
-        </div>
-      )}
-      <style jsx>{`
+	return (
+		<div className="min-h-screen bg-gray-200 py-4 flex flex-col justify-center sm:py-10">
+			<div className="relative py-1 sm:max-w-xl sm:mx-auto">
+				<div className="relative px-6 py-8 bg-white shadow-lg sm:rounded-lg sm:py-6 sm:px-4">
+					<form onSubmit={handleSubmit} className="max-w-md mx-auto">
+						<textarea
+							rows={2}
+							cols={20}
+							wrap=""
+							value={inputValue}
+							onChange={(e) => setInputValue(e.target.value)}
+							onFocus={handleTextareaFocus}
+							onBlur={handleTextareaBlur}
+							className="w-full px-3 py-2 text-gray-700 bg-gray-200 rounded focus:outline-none focus:ring focus:border-cyan-400"
+							placeholder="Enter your imagination..."
+						/>
+						<input
+							type="text"
+							value={negativeInput}
+							onChange={(e) => setnegativeInput(e.target.value)}
+							className="w-full px-5 py-3 text-gray-700 bg-gray-200 rounded focus:outline-none focus:ring focus:border-cyan-400"
+							placeholder="Enter a negative prompt"
+						/>
+						<button
+							type="submit"
+							className={`w-full mt-5 mb-1 px-3 py-2 text-white bg-gradient-to-r from-cyan-400 to-green-500 rounded-md focus:outline-none ${
+								isTextareaFocused ? "mt-2" : "mt-0"
+							}`}
+							disabled={loading}
+						>
+							Generate
+						</button>
+					</form>
+					<div className="flex justify-between -mb-2">
+						<Link
+							href="/image/refine"
+							className="text-gray-600 text-sm px-2 py-1 hover:underline hover:text-lime-700"
+						>
+							Refine an image &rarr;
+						</Link>
+						<Link
+							href="/image/detect"
+							className="text-gray-600 text-sm px-2 py-1 hover:underline hover:text-lime-700"
+						>
+							Detect an image &rarr;
+						</Link>
+						<Link
+							href="/image/answer"
+							className="text-gray-600 text-sm px-2 py-1 hover:underline hover:text-lime-700"
+						>
+							Answer question &rarr;
+						</Link>
+					</div>
+
+					<div className="flex justify-center -mb-2 mt-1">
+						<Link
+							href="/audio/transcriber"
+							className="text-gray-600 text-sm px-2 py-1 hover:underline hover:text-cyan-700"
+						>
+							Audio transcriber &rarr;
+						</Link>
+						<Link
+							href="/audio/generate"
+							className="text-gray-600 text-sm px-2 py-1 hover:underline hover:text-cyan-700"
+						>
+							Generate audio &rarr;
+						</Link>
+					</div>
+				</div>
+			</div>
+			{loading && (
+				<div className="mt-12 flex justify-center">
+					<div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12" />
+				</div>
+			)}
+			{imageUrl && !loading && (
+				<div className="relative mt-8 flex flex-col items-center justify-center gap-4">
+					<div className="group relative">
+						<Image
+							src={imageUrl}
+							alt="Generated image"
+							className="rounded-lg shadow-lg transition-shadow duration-300"
+							height={350}
+							width={350}
+						/>
+						<button
+							className="absolute bottom-4 right-4 bg-white font-semibold rounded-md p-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+							type="button"
+							onClick={downloadImage}
+						>
+							<MdOutlineDownloading size={20} color="black" />
+						</button>
+					</div>
+				</div>
+			)}
+			<style jsx>{`
         .loader {
           animation: spin 1s linear infinite;
-          border-top-color: #3498db;
+          border-top-color: #5BC880;
         }
 
         @keyframes spin {
@@ -140,8 +159,8 @@ function Home(): JSX.Element {
           }
         }
       `}</style>
-    </div>
-  )
+		</div>
+	);
 }
 
-export default Home
+export default Home;
--- utils/hf-handlers.ts
+++ utils/hf-handlers.ts
@@ -1,107 +1,107 @@
-import hf from "@/config/huggingFace"
-import { convertAudioToBlob, convertImageToBlob } from "./utils"
+import hf from "@/config/huggingFace";
+import { convertAudioToBlob, convertImageToBlob } from "./utils";
 
 export const generateImage = async (input: string, negativeInput: string) => {
-  try {
-    const output = await hf.textToImage({
-      model: "stabilityai/stable-diffusion-xl-base-1.0",
-      inputs: input,
-      parameters: {
-        negative_prompt: negativeInput,
-      },
-    })
+	try {
+		const output = await hf.textToImage({
+			model: "stabilityai/stable-diffusion-xl-base-1.0",
+			inputs: input,
+			parameters: {
+				negative_prompt: negativeInput,
+			},
+		});
 
-    return output
-  } catch (error) {
-    console.error(error)
-  }
-}
+		return output;
+	} catch (error) {
+		console.error(error);
+	}
+};
 
 export const refineImage = async (prompt: string, imageFile: File) => {
-  try {
-    if (!imageFile) return
+	try {
+		if (!imageFile) return;
 
-    const imageBlob = await convertImageToBlob(imageFile)
+		const imageBlob = await convertImageToBlob(imageFile);
 
-    const output = await hf.imageToImage({
-      inputs: imageBlob,
-      parameters: {
-        prompt: prompt,
-      },
-      model: "stabilityai/stable-diffusion-xl-refiner-1.0",
-    })
+		const output = await hf.imageToImage({
+			inputs: imageBlob,
+			parameters: {
+				prompt: prompt,
+			},
+			model: "stabilityai/stable-diffusion-xl-refiner-1.0",
+		});
 
-    return output
-  } catch (error) {
-    console.error(error)
-  }
-}
+		return output;
+	} catch (error) {
+		console.error(error);
+	}
+};
 
 export const detectImage = async (imageFile: File) => {
-  try {
-    if (!imageFile) return
+	try {
+		if (!imageFile) return;
 
-    const imageBlob = await convertImageToBlob(imageFile)
+		const imageBlob = await convertImageToBlob(imageFile);
 
-    const output = await hf.objectDetection({
-      data: imageBlob,
-      model: "facebook/detr-resnet-50",
-    })
+		const output = await hf.objectDetection({
+			data: imageBlob,
+			model: "facebook/detr-resnet-50",
+		});
 
-    return output
-  } catch (error) {
-    console.error(error)
-  }
-}
+		return output;
+	} catch (error) {
+		console.error(error);
+	}
+};
 
 export const answerQuestion = async (question: string, imageFile: File) => {
-  try {
-    if (!imageFile) return
+	try {
+		if (!imageFile) return;
 
-    const imageBlob = await convertImageToBlob(imageFile)
+		const imageBlob = await convertImageToBlob(imageFile);
 
-    const output = await hf.visualQuestionAnswering({
-      model: "dandelin/vilt-b32-finetuned-vqa",
-      inputs: {
-        question: question,
-        image: imageBlob,
-      },
-    })
+		const output = await hf.visualQuestionAnswering({
+			model: "dandelin/vilt-b32-finetuned-vqa",
+			inputs: {
+				question: question,
+				image: imageBlob,
+			},
+		});
 
-    return output
-  } catch (error) {
-    console.error(error)
-  }
-}
+		return output;
+	} catch (error) {
+		console.error(error);
+	}
+};
 
 export const transcribeAudio = async (audioFile: File) => {
-  try {
-    if (!audioFile) return
+	try {
+		if (!audioFile) return;
 
-    const audioBlob = await convertAudioToBlob(audioFile)
+		const audioBlob = await convertAudioToBlob(audioFile);
 
-    const output = await hf.automaticSpeechRecognition({
-      model: "openai/whisper-large-v3",
-      data: audioBlob,
-    })
+		const output = await hf.automaticSpeechRecognition({
+			model: "openai/whisper-large-v3",
+			data: audioBlob,
+		});
 
-    return output
-  } catch (error) {
-    console.error(error)
-  }
-}
+		return output;
+	} catch (error) {
+		console.error(error);
+	}
+};
 
 export const generateAudio = async (text: string) => {
-  try {
-    if (!text) return
+	try {
+		if (!text) return;
 
-    const output = await hf.textToSpeech({
-      model: "facebook/mms-tts-eng",
-      inputs: text,
-    })
+		const output = await hf.textToSpeech({
+			model: "facebook/mms-tts-eng",
+			inputs: text,
+		});
 
-    return output
-  } catch (error) {
-    console.error(error)
-  }
-}
+		return output;
+	} catch (error) {
+		console.error(error);
+	}
+};
